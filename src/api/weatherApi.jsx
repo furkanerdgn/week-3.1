68da198f40be525ecf93d1edf757b3c2a530737b
Furkan <furkanerdgn99@gmail.com>
2023-04-15 20:32:06 +0300

    secure layer

--- src/api/weatherApi.jsx
+++ src/api/weatherApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_OPENWEATHERMAP_API_KEY;
-const BASE_URL = "http://api.openweathermap.org/data/2.5";
+const BASE_URL = "https://api.openweathermap.org/data/2.5";
 
 export const fetchCityWithName = async (cityName) => {
   const url = `${BASE_URL}/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
